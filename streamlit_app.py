237f1e55c6cba046bf7371b2996d3faa9a711aaf
papz-i <torrecampopiolo@google.com>
2023-05-24 19:51:03 +0800

    streamlit.py added

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('model_best.h5')
+  return model
+model=load_model()
+st.write("""CATTLE IDENTIFICATION MODEL"""
+)
+file=st.file_uploader("Choose cattle photo from computer",type=["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Ayrshire cattle', 'Holstein Friesian cattle', 'Jersey cattle', 'Brown Swiss cattle', 'Red Dane cattle']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
